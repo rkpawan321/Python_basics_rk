b2fc66fa572e5c46ad8e431da860f2d76af36f35
rkpawan321 <rkpawan321@gmail.com>
2024-03-06 23:46:59 -0800

    intergers and float

---
+++ IntegersAndFloats.py
@@ -0,0 +1,10 @@
+x = int(4.7)   # x is now an integer 4 
+y = float(4)   # y is now a float of 4.0
+
+print(type(x))
+# int
+print(type(y))
+# float
+
+print(.1 + .1 + .1 == .3)
+# False
---
+++ VariablesAndAssigmentOperatoes.py
@@ -0,0 +1,34 @@
+my_population = 272728
+
+x = 2
+y = x
+print(y)
+
+x,y,z = 3,4,5
+
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+rainfall *= 0.9
+
+# add the rainfall variable to the reservoir_volume variable
+reservoir_volume += rainfall
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+reservoir_volume *= 1.05
+
+# into the reservoir in the days following the storm
+
+# decrease reservoir_volume by 5% to account for evaporation
+reservoir_volume *= 0.95
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+reservoir_volume -= 2.5e5
+
+# that's piped to arid regions.
+
+# print the new value of the reservoir_volume variable
+print(reservoir_volume)
